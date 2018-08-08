80bb14479a752dc3c073f5f2f4e8abca1cb03bac
Katarzyna Kensy <zbierek@gmail.com>
2018-08-08 15:24:36 +0200

    Beutify all .js files

--- components/Contact.js
+++ components/Contact.js
@@ -1,19 +1,28 @@
 var Contact = React.createClass({
-  propTypes: {
-    item: React.PropTypes.object.isRequired,
-  },
+    propTypes: {
+        item: React.PropTypes.object.isRequired,
+    },
 
-  render: function() {
-    return (
-      React.createElement('div', {className: 'contactItem'},
-        React.createElement('img', {
-          className: 'contactImage',
-          src: './img/user.png'
-        }),
-        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-        React.createElement('a', {className: 'contactEmail', href: "mailto: "  + this.props.item.email}, this.props.item.email)
-      )
-    )
-  },
+    render: function() {
+        return (
+            React.createElement('div', {
+                    className: 'contactItem'
+                },
+                React.createElement('img', {
+                    className: 'contactImage',
+                    src: './img/user.png'
+                }),
+                React.createElement('p', {
+                    className: 'contactLabel'
+                }, 'Imię: ' + this.props.item.firstName),
+                React.createElement('p', {
+                    className: 'contactLabel'
+                }, 'Nazwisko: ' + this.props.item.lastName),
+                React.createElement('a', {
+                    className: 'contactEmail',
+                    href: "mailto: " + this.props.item.email
+                }, this.props.item.email)
+            )
+        )
+    },
 });
--- components/Contacts.js
+++ components/Contacts.js
@@ -3,13 +3,18 @@ var Contacts = React.createClass({
         items: React.PropTypes.array.isRequired,
     },
 
-    render: function () {
-        var contacts = this.props.items.map(function (contact) {
-            return React.createElement(Contact, { item: contact, key: contact.id });
+    render: function() {
+        var contacts = this.props.items.map(function(contact) {
+            return React.createElement(Contact, {
+                item: contact,
+                key: contact.id
+            });
         });
 
         return (
-            React.createElement('ul', { className: 'contactsList' }, contacts)
+            React.createElement('ul', {
+                className: 'contactsList'
+            }, contacts)
         );
     }
 });
